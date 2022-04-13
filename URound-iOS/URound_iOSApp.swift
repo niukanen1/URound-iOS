de4278f757703f18264d7780cf46de8b8e2fdd11
Ivan Niukanen <ivanniu2003@gmail.com>
2022-04-13 17:32:04 +0300

    Initial Commit

---
+++ URound-iOS/URound_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  URound_iOSApp.swift
+//  URound-iOS
+//
+//  Created by Ivan Niukanen on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct URound_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
